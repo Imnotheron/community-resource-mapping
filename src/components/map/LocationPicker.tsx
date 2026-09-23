9c92d58977820277c1769dafe4418538dcc8e5f5
Imnotheron <legendxdplaciente@gmail.com>
2026-09-23 15:22:03 +0800

    fix: import effect for map coverage lock

--- src/components/map/LocationPicker.tsx
+++ src/components/map/LocationPicker.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
